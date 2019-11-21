e6d5e86c56b1b1dc91b2324a0f32f0fa96f35577
Robert Morse <rob.632@hotmail.co.uk>
2019-11-21 13:43:23 +0000

    set up flask train and predict apis

--- flask_app/app/index.py
+++ flask_app/app/index.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
-from flask import Flask
-from utils.train import train
-from utils.predict import predict
-
+from flask import Flask, request, jsonify
+import os
+#from utils.train import train
+#from utils.predict import predict
 
 
 app = Flask(__name__)
@@ -14,23 +14,34 @@ mydb = mongo_client['document_name']
 
 my_collection = mydb['collection_name']
 
+@app.route('/')
+def index():
+    return('flask homepage')
 
-@app.route('/train')
+#set up API POSt endpoint
+@app.route('/train', methods=['POST'])
 def train():
+    content = request.json
     #accept request
     #manipulate data into useable format
     #respond saying that the new model is being trained
     #write new data to database
     #retrain model
     #restore emodel
-    return()
+    return('return from train API')
 
 
-@app.route('/predict')
+@app.route('/predict', methods = ['POST'])
 def predict():
+    content = request.json
     #accept request
     #manipulate data into useable format
-    #load model 
+    #load model
     #predict
     #return prediction
-    return()
+    return('return from predict API')
+
+if __name__ ==	'__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port = port, debug = True)
